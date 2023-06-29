aa30c10e654bc01cd2fa43139e0af18ba712990f
Firmin_ <40271484+firminsurgithub@users.noreply.github.com>
2023-06-29 11:49:22 +0200

    🔁 Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+declare -a requirements=("sudo" "systemd" "wget")
+
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run as root. Please enter the root password to continue :"
     sudo -k
     sudo "$0" "$@"
     exit $?
 fi
+
+isInstalled() {
+  $1 --version &> /dev/null
+  if (($? != 0 ))
+  then 
+    echo -e"[${RED}FATAL${RESET}] $1 is not installed ! Please install it using your package manager first."
+    exit 1
+  fi
+}
+
+for i in ${requirements[@]};do
+  isInstalled $i
+done
 
 echo "Download bdsm-server-linux from GitHub..."
 wget -O /tmp/bdsm-server-linux https://github.com/firminsurgithub/bdsm-server/releases/latest/download/bdsm-server-linux
@@ -29,12 +44,11 @@ echo "Creation of the systemd bdsm-server service..."
 echo "[Unit]
 Description=BDSM Server unit service
 After=network.target
+
+[Service]
 Type=simple
 Restart=always
 RestartSec=1
-Description=bdsm-server
-
-[Service]
 ExecStart=/usr/share/bdsm-server/bdsm-server-linux
 WorkingDirectory=/usr/share/bdsm-server
 
